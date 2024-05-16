efc998972ca45ce9ea166832bed4cac72e7000fd
trey-rosius <treyrosius@gmail.com>
2024-05-16 14:12:02 +0100

    more updates

--- delivery-service/main.py
+++ delivery-service/main.py
@@ -2,6 +2,7 @@ import os
 
 from dapr.clients import DaprClient
 from fastapi import FastAPI, HTTPException
+from models.delivery_status_model import DeliveryStatusModel
 
 import grpc
 
@@ -28,19 +29,17 @@ class CloudEvent(BaseModel):
     traceid: str
 
 
-@app.post('/api/v1/delivery-service/{package_id}')
-def delivery_status_update(package_id: str):
+@app.post('/api/v1/delivery-service/update-package-status')
+def delivery_status_update(delivery_status_model: DeliveryStatusModel):
 
     with DaprClient() as d:
-        logging.info(f'package id is:{package_id}')
+        logging.info(f'package id is:{delivery_status_model.packageId}')
 
         try:
-            delivery_package = {
 
-            }
             d.save_state(store_name=delivery_db,
-                         key=package_id,
-                         value=package_model.model_dump_json())
+                         key=delivery_status_model.packageId,
+                         value=delivery_status_model.model_dump_json())
 
             return {"message": "package successfully created"}
         except grpc.RpcError as err:
