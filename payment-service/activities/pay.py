3dd14183099e96ca7cefb84ba13ae56d373c6d45
trey-rosius <treyrosius@gmail.com>
2024-06-04 23:34:07 +0100

    more updates

---
+++ payment-service/activities/pay.py
@@ -0,0 +1,14 @@
+import json
+
+from dapr.clients import DaprClient
+
+
+def process(input: any):
+    input['payment_processed'] = True
+    try:
+        with DaprClient() as d:
+            d.save_state(store_name="statestore", key=input['id'] + "-payment", value=json.dumps(input))
+    except Exception as e:
+        print(e, flush=True)
+
+    return input
--- payment-service/main.py
+++ payment-service/main.py
@@ -12,6 +12,8 @@ from models.payment_model import PaymentModel
 import logging
 from stripe import StripeClient
 import dapr.ext.workflow as wf
+import activities
+
 
 app = FastAPI()
 base_url = os.getenv('DAPR_HTTP_ENDPOINT', 'http://localhost')
@@ -21,7 +23,6 @@ stripe_secret = os.getenv('STRIPE_SECRET_KEY', '')
 
 logging.basicConfig(level=logging.INFO)
 
-app = FastAPI()
 client = StripeClient(stripe_secret)
 logging.basicConfig(level=logging.INFO)
 wfr = wf.WorkflowRuntime()
@@ -29,151 +30,65 @@ wf_client = wf.DaprWorkflowClient()
 wfr.start()
 
 
-@app.get('/v1.0/payments/{payment_intent}/confirm')
-def confirm_payment_intent(payment_intent: str):
-    with DaprClient as d:
-
-        print(f'payment intent: Received input: {payment_intent}.')
-        try:
-            kv = d.get_state(payments_db, payment_intent);
-            print(f"value of kv is {kv.data}")
-            if kv.data:
-                payment_model = PaymentModel(**json.loads(kv.data))
-                try:
-                    payment_confirmation = client.payment_intents.confirm(payment_intent,
-                                                                          params={
-                                                                              "payment_method": "pm_card_visa",
-                                                                              "return_url": "https://www.example.com",
-                                                                          }
-
-                                                                          )
-                    d.raise_workflow_event()
-                    return {"status": payment_confirmation['status'],
-                            }
-
-                except StripeError as err:
-
-                    raise HTTPException(status_code=500, detail=err.user_message)
-
-        except grpc.RpcError as err:
-
-            raise HTTPException(status_code=500, detail=err.details())
-
-
-
-
-
-@app.get('/v1.0/payments/{payment_intent}/cancel')
-def cancel_payment_intent(payment_intent: str):
-    print(f'cancel payment intent: Received input: {payment_intent}.')
-    try:
-        payment_cancel = client.payment_intents.cancel(payment_intent
-
-                                                       )
-        return {"cancel": payment_cancel,
-
-                }
-
-    except StripeError as err:
-
-        raise HTTPException(status_code=500, detail=err.user_message)
 
 
 @app.post('/v1.0/payment')
 def initiate_payment(payment: PaymentModel):
-    instance_id = wf_client.schedule_new_workflow(workflow=payment_workflow, input=payment)
+    instance_id = wf_client.schedule_new_workflow(workflow=task_chain_workflow, input={"first_name": "steve",
+                                                                                    "last_name": "rose",
+                                                                                    "lcoation": "douala",
+                                                                                       "car_class":"compact"})
 
-    wf_client.wait_for_workflow_completion(instance_id)
-    return "success"
+    wf_client.wait_for_workflow_completion(instance_id, timeout_in_seconds=60)
+    return "Done"
 
-
-@wfr.workflow(name='makePayment')
-def payment_workflow(ctx: wf.DaprWorkflowContext, wf_input: PaymentModel):
+@wfr.workflow(name='reserveflight')
+def task_chain_workflow(ctx: wf.DaprWorkflowContext, wf_input: any):
     try:
-        wf_input.instance_id = ctx.instance_id
-        payment_intent_response = yield ctx.call_activity(create_payment_intent, input=wf_input)
-        if payment_intent_response["status"] == "success":
-            approval_task = ctx.wait_for_external_event("approve payment")
-            timeout_event = ctx.create_timer(timedelta(seconds=200))
-            winner = yield wf.when_any([approval_task, timeout_event])
-            if winner == timeout_event:
-                yield ctx.call_activity(send_notification_activity,
-                                        input={
-                                            "status": "failed",
-                                            "message": f"Your payment with id {payment_intent_response['payment_intent_id']} timed out"
-                                        })
-                return
-            approval_result = yield approval_task
-            if approval_result["succeeded"]:
-                yield ctx.call_activity(send_notification_activity,
-                                        input={
-                                            "status": "succeeded",
-                                            "message": f"Your payment with id {payment_intent_response['payment_intent_id']} succeeded"
-                                        })
-            else:
-                yield ctx.call_activity(send_notification_activity,
-                                        input={
-                                            "status": "Cancelled",
-                                            "message": f"Your payment with id {payment_intent_response['payment_intent_id']} called"
-                                        })
+        reserveFlightResponse = yield ctx.call_activity(reserve_flight, input=wf_input)
+        reserveCarResponse = yield ctx.call_activity(reserve_car, input=reserveFlightResponse)
+        processPaymentResponse = yield ctx.call_activity(process_payment, input=reserveCarResponse)
+        confirmFlightResponse = yield ctx.call_activity(confirm_flight, input=processPaymentResponse)
+        confirmCarResponse = yield ctx.call_activity(confirm_car, input=confirmFlightResponse)
+        notifyResponse = yield ctx.call_activity(notify_success, input=confirmCarResponse)
 
-
+        print(notifyResponse, flush=True)
     except Exception as e:
         yield ctx.call_activity(error_handler, input=str(e))
         raise
-    return [create_payment_intent, send_notification_activity, error_handler]
+    return [reserveFlightResponse, reserveCarResponse, processPaymentResponse, confirmFlightResponse, confirmCarResponse, notifyResponse]
 
+
+@wfr.activity
+def reserve_flight(ctx, activity_input):
+    print(f'Reserve Flight: Received input: {activity_input}.')
+    return activities.reserve.flight(activity_input)
+
+@wfr.activity
+def reserve_car(ctx, activity_input):
+    print(f'Reserve Car: Received input: {activity_input}.')
+    return activities.reserve.car(activity_input)
+
+@wfr.activity
+def process_payment(ctx, activity_input):
+    print(f'Process Payment: Received input: {activity_input}.')
+    return activities.pay.process(activity_input)
+
+@wfr.activity
+def confirm_flight(ctx, activity_input):
+    print(f'Confirm Flight: Received input: {activity_input}.')
+    return activities.confirm.flight(activity_input)
+
+@wfr.activity
+def confirm_car(ctx, activity_input):
+    print(f'Confirm Car: Received input: {activity_input}.')
+    return activities.confirm.car(activity_input)
+
+@wfr.activity
+def notify_success(ctx, activity_input):
+    print(f'Notify On Success: Received input: {activity_input}.')
+    return activities.success.notify(activity_input)
 
 @wfr.activity
 def error_handler(ctx, error):
     print(f'Executing error handler: {error}.')
-
-
-@wfr.activity
-def create_payment_intent(ctx, activity_input: PaymentModel):
-    with DaprClient as d:
-        try:
-            print(f'create payment intent: Received input: {activity_input}.')
-            payment_intent = client.payment_intents.create(
-                params={
-                    "amount": activity_input.amount,
-                    "currency": "usd"
-
-                }
-
-            )
-            activity_input.id = payment_intent.id
-            activity_input.payment_intent_id = payment_intent.id
-            d.save_state(store_name=payments_db,
-                         key=activity_input.id,
-                         value=activity_input.model_dump_json(),
-                         state_metadata={"contentType": "application/json"})
-
-            print(f'created payment intent: {activity_input}')
-
-            return {
-                "payment_intent_id": payment_intent.id,
-                "status": "success"
-            }
-        except grpc.RpcError as err:
-            print(f"Error={err.details()}")
-            raise HTTPException(status_code=500, detail=err.details())
-
-
-
-
-
-@wfr.activity
-def send_notification_activity(ctx, activity_input):
-    with DaprClient as d:
-        '''
-          d.publish_event(
-            pubsub_name=pubsub_name,
-            topic_name=topic_name,
-            data=json.dumps(activity_input),
-            data_content_type='application/json',
-        )
-        print(f"received notification activity: {activity_input}")
-        return "success"
-        '''
-        print(f"received notification activity: {activity_input}")
